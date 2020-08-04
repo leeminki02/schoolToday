14a1707be340dcc092b3518c8f9c5483220a8b36
leeminki02 <type4892@gmail.com>
2020-08-04 22:24:22 +0900

    fetch-data 전 backup용 script

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,49 @@
-import 'package:flutter/material.dart';
+import 'dart:html';
 
-void main() {
-  runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Meal {
+  final String foodsList;
+  final String calories;
+  final String nutritions;
+
+  Meal({this.foodsList, this.calories, this.nutritions});
+
+  factory Meal.fromJson(Map<String, dynamic> json) {
+    return Meal(
+      foodsList: json['DDISH_NM'],
+      calories: json['CAL_INFO'],
+      nutritions: json['NTR_INFO'],
+    );
+  }
 }
 
-class MyApp extends StatelessWidget {
+Future<Meal> fetchMeals(lord) async {
+  final response = await http.get(
+      'https://open.neis.go.kr/hub/mealServiceDietInfo?Type=json&KEY=f8695107c89049538bf22c059faeb9a1&MLSV_YMD=20200805&ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7240085&MMEAL_SC_CODE=' +
+          lord);
+  if (response.statusCode == 200) {
+    return Meal.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed');
+  }
+}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
   // This widget is the root of your application.
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+/*
+class _MyAppState extends State<MyApp>{
+
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
@@ -18,7 +55,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
+*/
 // class MyHomePage extends StatefulWidget {
 //   MyHomePage({Key key, this.title}) : super(key: key);
 //   final String title;
@@ -26,10 +63,26 @@ class MyApp extends StatelessWidget {
 //   _MyHomePageState createState() => _MyHomePageState();
 // }
 
-class MyHomePage extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  Future<Meal> futureLunch, futureDinner;
+
+  @override
+  void initState() {
+    super.initState();
+    futureLunch = fetchMeals('2');
+    futureDinner = fetchMeals('3');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Scaffold(
+            /*
         appBar: AppBar(
           title: Text(
             '이민기님의 학교생활',
@@ -38,59 +91,190 @@ class MyHomePage extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.white,
         ),
-        body: Center(
-          // heightFactor: 0.9,
-          child: FractionallySizedBox(
-            widthFactor: 0.8,
-            child: Card(
-              margin: EdgeInsets.only(top: 20, bottom: 20),
-              child: Column(
-                children: <Widget>[
-                  Text('Hello 1'),
-                  Text('Hello 2'),
-                  Text('Hello 3'),
-                  Text('Hello 4'),
-                ],
+        */
+            body: SafeArea(
+              child: SingleChildScrollView(
+                // heightFactor: 0.9,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Container(
+                        padding: EdgeInsets.all(20),
+                        color: Colors.blue,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text('안녕하세요, 이민기님!',
+                                style: TextStyle(
+                                    fontSize: 15, color: Colors.white)),
+                            Text('2020.08.04. 화요일',
+                                style: TextStyle(
+                                    fontSize: 30, color: Colors.white)),
+                            Text('기말고사(1,3학년, 至 8/4)',
+                                style: TextStyle(
+                                    // fontStyle: FontStyle.italic,
+                                    fontSize: 20,
+                                    color: Colors.white))
+                          ],
+                        )),
+                    Card(
+                      elevation: 5,
+                      margin: EdgeInsets.all(10),
+                      color: Colors.green,
+                      child: Container(
+                        child: Column(
+                          children: <Widget>[
+                            Text('날씨 예보'),
+                            Text('교내 기상관측 정보'),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 5,
+                      margin: EdgeInsets.all(10),
+                      color: Colors.amber,
+                      child: Container(child: Text('시간표')),
+                    ),
+                    Card(
+                      elevation: 5,
+                      margin: EdgeInsets.all(10),
+                      color: Colors.brown,
+                      child: Container(
+                          child: FutureBuilder<Meal>(
+                              future: futureLunch,
+                              builder: (context, snapshot) {
+                                if (snapshot.hasData) {
+                                  return Text(snapshot.data.foodsList);
+                                } else if (snapshot.hasError) {
+                                  return Text("${snapshot.error}");
+                                }
+
+                                return CircularProgressIndicator();
+                              })),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
+            bottomNavigationBar: BottomAppBar(
+              color: Colors.white,
+              child: Row(
+                // mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.wb_sunny),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.calendar_today),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.hourglass_empty),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.alarm),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            )));
+  }
+}
 
-          /*
-          Card(
+/*
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        /*
+        appBar: AppBar(
+          title: Text(
+            '이민기님의 학교생활',
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+        ),
+        */
+        body: SafeArea(
+          child: SingleChildScrollView(
+            // heightFactor: 0.9,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                // Container(
-                FractionallySizedBox(
-                    // alignment: Alignment.center,
-                    widthFactor: 1.0,
-                    child: Container(
-                      height: 150,
-                      color: Colors.blueAccent[100],
-                      child: Text('맑음'),
-                    ))
+                Container(
+                    padding: EdgeInsets.all(20),
+                    color: Colors.blue,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('안녕하세요, 이민기님!',
+                            style:
+                                TextStyle(fontSize: 15, color: Colors.white)),
+                        Text('2020.08.04. 화요일',
+                            style:
+                                TextStyle(fontSize: 30, color: Colors.white)),
+                        Text('기말고사(1,3학년, 至 8/4)',
+                            style: TextStyle(
+                                // fontStyle: FontStyle.italic,
+                                fontSize: 20,
+                                color: Colors.white))
+                      ],
+                    )),
+                Card(
+                  elevation: 5,
+                  margin: EdgeInsets.all(10),
+                  color: Colors.green,
+                  child: Container(
+                    child: Column(
+                      children: <Widget>[
+                        Text('날씨 예보'),
+                        Text('교내 기상관측 정보'),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 5,
+                  margin: EdgeInsets.all(10),
+                  color: Colors.amber,
+                  child: Container(child: Text('시간표')),
+                ),
+                Card(
+                  elevation: 5,
+                  margin: EdgeInsets.all(10),
+                  color: Colors.brown,
+                  child: Container(
+                    child: Text('급식 식단'),
+                  ),
+                ),
               ],
             ),
-            margin: EdgeInsets.all(20),
           ),
-*/
         ),
-        /*floatingActionButton: FloatingActionButton(
+        /*
+        floatingActionButton: FloatingActionButton(
           onPressed: null,
-          tooltip: 'Increment',
           child: Icon(
             Icons.settings,
             color: Colors.white,
           ),
           backgroundColor: Colors.grey,
-        ),*/
+        ),
+        */
         // floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
         bottomNavigationBar: BottomAppBar(
           color: Colors.white,
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            // mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               IconButton(
-                icon: Icon(Icons.local_dining),
+                icon: Icon(Icons.wb_sunny),
                 onPressed: () {},
               ),
               IconButton(
@@ -98,16 +282,23 @@ class MyHomePage extends StatelessWidget {
                 onPressed: () {},
               ),
               IconButton(
-                icon: Icon(Icons.wb_sunny),
+                icon: Icon(Icons.hourglass_empty),
                 onPressed: () {},
               ),
               IconButton(
-                icon: Icon(Icons.schedule),
+                icon: Icon(Icons.alarm),
                 onPressed: () {},
               ),
+              /*
+              SizedBox( //dummy box for floatingbtn if needed
+                width: 40,
+                child: null,
+              )
+              */
             ],
           ),
           // shape: CircularNotchedRectangle(),
         ));
   }
 }
+*/
